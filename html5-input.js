097f5dea7a1531c83573416c70c08026ae8cbdec
jamescarr <jamescarr@abidos.(none)>
2009-06-05 15:42:10 -0500

    added blur functionality

--- html5-input.js
+++ html5-input.js
@@ -5,14 +5,16 @@ carrcraft.Html5Emulator = function(){
 carrcraft.Html5Emulator.prototype.emulateInputs = function(){
   $('input').each(function(){
     var input = $(this);
-    if($(input).attr('placeholder')){
+    if(input.attr('placeholder')){
       input.val(input.attr('placeholder'))
       input.focus(function(){
         if(input.val() == input.attr('placeholder')){
           $(input).val("");      
         }
       }).blur(function(){
-        
+        if(input.val()==""){
+          input.val(input.attr('placeholder'));      
+        }
       })
     }
   });
